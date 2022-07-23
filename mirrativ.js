6856893e146be5a29ae4a452a6b670a56506c111
NaeCqde <50701386+NaeCqde@users.noreply.github.com>
2022-07-23 15:52:36 +0900

    Update mirrativ.js

--- mirrativ.js
+++ mirrativ.js
@@ -1,21 +1,62 @@
-/*ここにMirrativ配信URLを貼り付ける*/
-const haisinurl = ""
+/*
+ここに行ってこのコード全て貼り付けてcheck連打してください
+https://app.checklyhq.com/checks/new/browser?framework=puppeteer
+*/
 
+const haisinurl = "" // mirrativ配信url ここにアクセスしに行きます
+const cooldown = 15000 // クールダウンが終わるまでの秒数
+const kaisu = 25 // 1回のクールダウンまでに動く回数
+
+
+var timer = 0;
 const puppeteer = require('puppeteer');
 
 (async () => {
-  console.log("New Browser")
-  const browser = await puppeteer.launch()
-  console.log("newpage")
-  const page = await browser.newPage()
-  console.log("Go To 配信ページ")
-  await page.goto(haisinurl)
-  await page.setViewport({ width: 1, height: 1 })
-  while (true) {
-    console.log("クッキー削除")
-    const client = await page.target().createCDPSession()
-    await client.send('Network.clearBrowserCookies')
-    console.log("ページリロード")
-    await page.reload();
-  }
+    await Promise.all([
+        miru1(),
+        miru1(),
+    ]).catch(error => logerror(error));
 })()
+
+async function miru1() {
+    await loglog("New Browser");
+    const browser = await puppeteer.launch();
+    await loglog("newpage");
+    const page = await browser.newPage();
+    await loglog("Go To 配信ページ");
+    await page.goto(haisinurl);
+    await page.setViewport({ width: 1280, height: 720 });
+    await Promise.all([
+    miru2(page),
+    miru2(page),
+    miru2(page),
+    miru2(page),
+    aInterval(),
+])
+}
+
+
+async function miru2(page) {
+    const client = await page.target().createCDPSession();
+    while (timer < kaisu) {
+        timer += 1
+        await client.send('Network.clearBrowserCookies');
+        await loglog("クッキー削除");
+        await page.reload();
+        await loglog("ページリロード")
+    };
+ };
+
+async function aInterval() {
+    setInterval(function() {
+        timer-=26;
+    }, cooldown);
+};
+
+async function loglog(message) {
+    console.log(`[${new Date().toLocaleString()}] [LOG]\t${message}\n`);
+}
+
+async function logerror(message) {
+     console.error(`\x1b[31m[${new Date().toLocaleString()}] [ERROR]\t${message instanceof Error ? `${message.name}\n\t${message.message}\n\t${message.stack}` : message}\x1b[0m\n`);
+}
